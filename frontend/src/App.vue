ea6332f84f36049af32a724f3a7e505775574785
Lukas Ehnle <lukas@owlabout.de>
2020-03-22 01:46:59 +0100

    feat: add icon font

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,31 @@
 <template>
   <div id="app">
     <div id="nav">
-      <img src="./assets/logo.svg" alt="logo">
-      <router-link to="/">Dashboard</router-link>
+      <img src="./assets/logo.svg" alt="logo" />
+      <router-link to="/" class="dashboard">Dashboard</router-link>
       <router-link to="/kurse">Kurse</router-link>
       <router-link to="/raeume">Räume</router-link>
       <router-link to="/hilfe">Hilfe</router-link>
-      <input type="search">
-      <router-link to="/profil">Dein Profil <img src="./assets/defaultAvatar.svg" alt="avatar"></router-link>
+      <input type="search" />
+      <router-link to="/profil">
+        Dein Profil <img src="./assets/defaultAvatar.svg" alt="avatar" />
+      </router-link>
     </div>
     <router-view />
   </div>
 </template>
 
-
 <style lang="scss">
-
 #nav {
-  box-shadow: 0px 4px 11px $color-shadow; 
+  box-shadow: 0px 4px 11px $color-shadow;
   height: 60px;
   display: flex;
   justify-content: space-around;
   align-items: center;
+
+  .dashboard:before {
+    font-icon: url("@/assets/logo.svg");
+  }
 
   a {
     text-decoration: none;
@@ -38,7 +42,6 @@
       background: $primary-gradient;
       color: #fff;
     }
-
   }
 }
 </style>
