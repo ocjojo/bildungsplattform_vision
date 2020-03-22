57bb1aa21c4e0616fc3b56e0080fbf3d581ff3ef
becca <becca@owlabout.de>
2020-03-22 01:48:01 +0100

    add sidebar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,38 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <img src="./assets/logo.svg" alt="logo">
+    <div class="nav">
+      <img src="./assets/logo.svg" alt="logo" />
       <router-link to="/">Dashboard</router-link>
       <router-link to="/kurse">Kurse</router-link>
       <router-link to="/raeume">Räume</router-link>
       <router-link to="/hilfe">Hilfe</router-link>
-      <input type="search">
-      <router-link to="/profil">Dein Profil <img src="./assets/defaultAvatar.svg" alt="avatar"></router-link>
+      <input type="search" />
+      <router-link to="/profil">
+        Dein Profil
+        <img src="./assets/defaultAvatar.svg" alt="avatar" />
+      </router-link>
     </div>
-    <router-view />
+    <SideBar />
+    <div class="content-container">
+      <router-view />
+    </div>
   </div>
 </template>
 
+<script>
+import SideBar from "./components/SideBar";
+
+export default {
+  components: {
+    SideBar
+  }
+};
+</script>
+
 
 <style lang="scss">
-
-#nav {
-  box-shadow: 0px 4px 11px $color-shadow; 
+.nav {
+  box-shadow: 0px 4px 11px $color-shadow;
   height: 60px;
   display: flex;
   justify-content: space-around;
@@ -38,7 +53,14 @@
       background: $primary-gradient;
       color: #fff;
     }
-
   }
+}
+
+.content-container {
+  position: absolute;
+  left: 270px;
+  top: 60px;
+  width: calc(100vw - 270px);
+  height: calc(100vh - 60px);
 }
 </style>
