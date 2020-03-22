fd232e2db5a59c870dc88126a919dd8825edeffd
Lukas Ehnle <lukas@owlabout.de>
2020-03-22 11:17:52 +0100

    feat: add login and router guards

---
+++ frontend/src/views/Main.vue
@@ -0,0 +1,101 @@
+<template>
+  <div id="app">
+    <div class="nav">
+      <img src="@/assets/logo.svg" alt="logo" />
+      <router-link to="/" class="dashboard">Dashboard</router-link>
+      <router-link to="/kurse" class="track">Kurse</router-link>
+      <router-link to="/raeume" class="community">Räume</router-link>
+      <router-link to="/hilfe" class="help">Hilfe</router-link>
+      <div class="search"><input type="search" /></div>
+      <router-link to="/profil">
+        Dein Profil
+        <img v-if="user" :src="user.profileImgTarget" alt="avatar" />
+        <img v-else src="@/assets/defaultAvatar.svg" alt="avatar" />
+      </router-link>
+    </div>
+    <SideBar />
+    <div class="content-container">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import SideBar from "@/components/SideBar";
+import { mapGetters } from "@/store";
+
+export default {
+  asyncComputed: {
+    ...mapGetters(["user"])
+  },
+  components: {
+    SideBar
+  }
+};
+</script>
+
+<style lang="scss">
+.nav {
+  box-shadow: 0px 4px 11px $color-shadow;
+  height: 60px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  .dashboard:before {
+    font-icon: url("@/assets/icons/dashboard.svg");
+  }
+  .community:before {
+    font-icon: url("@/assets/icons/community.svg");
+  }
+  .help:before {
+    font-icon: url("@/assets/icons/help.svg");
+  }
+  .search {
+    position: relative;
+  }
+  .search:before {
+    font-icon: url("@/assets/icons/search.svg");
+    position: absolute;
+    right: 20px;
+    top: 50%;
+    transform: translateY(-50%);
+    font-size: 1.2rem;
+    color: $primary;
+  }
+  .track:before {
+    font-icon: url("@/assets/icons/track.svg");
+  }
+
+  a {
+    text-decoration: none;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    padding: 15px;
+
+    img {
+      margin-left: 10px;
+    }
+    &:before {
+      margin-right: 5px;
+      color: $primary;
+    }
+    &.router-link-exact-active {
+      background: $primary-gradient;
+      color: #fff;
+      &:before {
+        color: inherit;
+      }
+    }
+  }
+}
+
+.content-container {
+  position: absolute;
+  left: 270px;
+  top: 60px;
+  width: calc(100vw - 270px);
+  height: calc(100vh - 60px);
+}
+</style>
---
+++ frontend/src/views/Register.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Registrierung</div>
+</template>
